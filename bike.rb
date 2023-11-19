cfabc0ae91d9213b545e74eb25d2b890bb6b616c
Kameron Beamer <beamerkameron@gmail.com>
2023-11-19 10:05:45 -0800

    Changed code to support poodr

--- bike.rb
+++ bike.rb
@@ -2,38 +2,19 @@
 
 class Bike
 
-  STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
+  attr_accessor :id, :color, :price, :weight, :rented
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
-
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false, storage)
     @id = id
     @color = color
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @storage = storage
   end
 
   def rent!
     self.rented = true
-  end
-
-  def add_cargo(item)
-    self.cargo_contents << item
-  end
-
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
-  end
-
-  def pannier_capacity
-    MAX_CARGO_ITEMS
-  end
-
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
   end
 
 end
